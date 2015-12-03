9846bd9281d45bed23649f9d9fd46f1b90c8ebad
Mo Moulton <mo.moulton22@gmail.com>
2015-12-03 15:37:07 -0500

    ballots table

---
+++ database/migrations/2015_12_03_200523_create_ballots_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBallotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ballots', function (Blueprint $table) {
+
+            # Increments method will make a Primary, Auto-Incrementing field.
+            # Most tables start off this way
+            $table->increments('id');
+
+            # This generates two columns: `created_at` and `updated_at` to
+            # keep track of changes to a row
+            $table->timestamps();
+
+            # The rest of the fields...
+            $table->integer('meeting_id')->unsigned();
+            $table->foreign('meeting_id')->references('id')->on('meetings');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ballots');
+    }
+}
